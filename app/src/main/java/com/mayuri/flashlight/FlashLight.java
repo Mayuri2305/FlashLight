d39784d83231fed8a1b5801215a3cc9b3987a0bb
USER <mayuri@tecnesium.com>
2017-09-05 15:13:54 +0530

    Initial commit

---
+++ app/src/main/java/com/mayuri/flashlight/FlashLight.java
@@ -0,0 +1,13 @@
+package com.mayuri.flashlight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlashLight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flash_light);
+    }
+}
